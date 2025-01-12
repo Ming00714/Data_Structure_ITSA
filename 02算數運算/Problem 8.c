90cec662ef845985ed5a48c6aba78ff4dde4b848
Ming00714 <163003004+Ming00714@users.noreply.github.com>
2025-01-13 01:43:44 +0800

    Create Problem 8.c

---
+++ 02算數運算/Problem 8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a, n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a);
+        printf("%d %d %d\n", a, a*a, a*a*a);
+    }
+
+    return 0;
+}
