6ded34b5c4d10836102ece95746cd60cf57c0f04
Ming00714 <163003004+Ming00714@users.noreply.github.com>
2025-01-13 01:26:45 +0800

    Create Problem 5.c

---
+++ 02算數運算/Problem 5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    printf("%d+%d=%d\n", a, b, a+b);
+    printf("%d*%d=%d\n", a, b, a*b);
+    printf("%d-%d=%d\n", a, b, a-b);
+
+    int c = a % b;
+    if(c < 0) printf("%d/%d=%d...%d\n", a, b, a/b-1, a%b+b);
+    else printf("%d/%d=%d...%d\n", a, b, a/b, a%b);
+
+    return 0;
+}
