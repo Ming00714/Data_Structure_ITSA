baca2ecceac56ccbaadc9a28052e4e74a4ddd0e5
Ming00714 <163003004+Ming00714@users.noreply.github.com>
2025-01-13 02:20:05 +0800

    Update Problem_13.c

--- 02算數運算/Problem_13.c
+++ 02算數運算/Problem_13.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+    float w, h;
+    scanf("%f %f", &w, &h);
+    h = h / 100;
+    printf("%0.2f\n", round(w / h / h * 100) / 100.00);
+    
+    return 0;
+}
+
+// BMI計算
