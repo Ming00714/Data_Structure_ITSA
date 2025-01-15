b1d690570b91eb221050f6eba2b898bc17ebeb38
Ming00714 <163003004+Ming00714@users.noreply.github.com>
2025-01-15 15:04:57 +0800

    Create C_MM15.c

---
+++ C_MM15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+    
+    if(x >= 0 && x <=100 && y >= 0 && y <= 100) printf("inside\n");
+    else printf("outside\n");
+
+ 
+    return 0;
+}
+
+// 判斷座標是否在正方形的範圍內
